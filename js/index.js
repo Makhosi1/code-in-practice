89bd607080c6bd7505359b081fce3c5bc782473d
Coco <admin@Makhosi-MacBook-Pro.local>
2023-11-11 13:45:01 +0000

    code in practice react

---
+++ js/index.js
@@ -0,0 +1,273 @@
+// Session 1 
+
+let date = "30.09.2023";
+let sesnumber = 2;
+console.log(date);
+console.log(sesnumber);
+
+console.log(date + ""+sesnumber);
+
+/*let topic = "I am doing a code in practice session";
+let age = 33;
+console.log(topic);
+
+age=20;
+console.log(age); */
+
+function subtract (num1, num2){
+    //return num1-num2;
+    console.log(num1-num2);
+
+}
+subtract(40,7);
+
+function division (num1,num2){
+    //return num1/num2;
+    console.log(num1/num2);
+
+}
+
+division(100,2);
+division(30,2)
+
+
+function concatenate (str1,str2){
+    console.log(str1 + " and " + str2);
+}
+
+concatenate("happy","joy");
+
+//let example= 1990;
+//console.log(example);
+
+
+function example (string3){
+    console.log(string3);
+}
+
+
+
+example("new is a statement for my function");
+
+
+function multiply(numb1,numb2){
+    //return (numb1*numb2);
+    console.log(numb1*numb2);
+
+}
+
+
+multiply(6,9);
+
+
+
+
+// Immediately-invoked function expression
+(function(){
+    // code goes here
+
+}())
+
+//arrow function 
+//const multiply = () => {
+    //return 2 * 2
+
+//}
+// you can copy online the arrow function but be careful of the variable (e.g multiply) above.
+
+// to minise the arrow function
+const multiplly = () => 2*2;
+
+console.log(multiplly());
+
+
+// Session 3 
+
+function checkEvenOrOdd (num) {
+    if (num % 2 === 0){
+        console.log(+num+ " is an even number");
+    } else {
+        console.log(+num+ " is an odd number");
+    }
+}
+
+checkEvenOrOdd(4);
+checkEvenOrOdd(763);
+
+/*for( let timer = 10; timer >= 0; timer-- ) {
+    if (timer === 0) {
+        console.log('Blast off');
+    }else {
+        console.log(timer);
+    }
+
+}*/
+
+// another way to solve the issue 
+function countDown() {
+    for ( let timer = 10; timer >= 1; timer-- ) {
+        console.log(timer);
+    }
+    console.log("Blast off");
+}
+ countDown();
+
+ const shoppingCart = [25, 35,60,45,10];
+ let sum = 0;
+
+ function calculateSum() {
+ for(let i = 0; i<shoppingCart.length; i++) {
+    sum = sum + shoppingCart[i];
+    console.log(sum);
+ }
+}
+calculateSum();
+
+const firstName = ['Makhosi', 'Coco', 'Love', "Honey", 'Joe'];
+let greeting ='Good, morning';
+
+function firstThing() {
+for(let j=0; j< firstName.length; j++){
+    console.log(greeting +' ' + firstName[j]);
+}
+}
+
+firstThing();
+
+// for the last element of an array the index with always be [array.length -1], this is how to access the last part of an array.*/
+
+
+
+
+// Code in Practice assignements 
+
+//fizzBuzz assignment
+
+let theRange = 1-100
+
+function fizzBuzz (a = 1-100){
+    for(a=0; a <= 100; a+=3) {
+        console.log("fizz " + theRange[a]);
+    }
+} console.log("Fizz");
+
+fizzBuzz();
+
+// *** End of assignement 
+
+
+//Week 1 assignment 
+
+
+
+function calculateAverage(testScore1,testScore2,testScore3){
+    //return (testScore1 + testScore2 + testScore3) / 3;
+    console.log((testScore1 + testScore2 + testScore3) / 3);
+
+}
+
+calculateAverage(35,45,25);
+calculateAverage(10,10,80);
+
+// *** End of assignement 
+
+
+// Week 2 Assignment.
+
+// Task 1
+
+let age = 33;
+const votingAge = 18; 
+ 
+function canVote(){
+    if (age >= 18){
+        console.log("You are " + age + " years old and can vote.");
+    } else
+        console.log("You are " + age + " years old and can't vote at this time.");
+}
+
+canVote();
+age =14;
+canVote();
+
+// Task 2 
+
+const favFruits = ['apples', 'kiwi\'s', 'mangoes', 'bananas', 'strawberries'];
+const saying = "My favorite fruits are ";
+
+function listOfFruits () {
+    for(c=0; c<favFruits.length; c++){
+        console.log(saying + ' '+  favFruits[c] +'.');
+    }
+}
+listOfFruits();
+
+// *** End of assignment.
+
+// 14/10/23 Java session 3
+
+// Arrays 
+
+// to add new elements in an array 
+const numsArray = [1,2 , 4, 5];
+
+let newElementPosition =2; 
+
+let newElementToInsert = 3;
+
+numsArray.splice(newElementPosition,0, newElementToInsert);
+
+console.log(numsArray);
+
+// to remove elements from an array
+
+let position = 3;
+numsArray.splice(position,1);
+console.log(numsArray);
+
+// use a button to change the body background of an HTML page 
+
+
+/*const changeButton = document.querySelector("#btn-change");
+
+function randomColors(){
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+ changeButton.addEventListener("click", function(){
+    document.body.style.background = randomColors();
+ });*/
+
+ // for linear gradient do it on the function randomcolors and on return line.
+
+ const messageForm = document.querySelector("form");
+ const messageInput = document.querySelector("#message");
+ const outputDiv = document.querySelector("#output");
+
+ messageForm.addEventListener("submit", function(e){
+    // this prevent the default loading of form when button is clicked
+    e.preventDefault();
+
+    //access the value form the input
+    const message = messageInput.value;
+
+    const paragraph = document.createElement('p');
+    paragraph.textContent = `Message: ${message}`;
+
+    //add paragraph to the div
+    outputDiv.append(paragraph);
+
+    // it will clear the input box
+    messageInput.value ="";
+
+    return false;
+
+ });
+
+// 21/10/2023 
+// JavaScript session 4 
+
+
+
---
+++ react-CID/index.js
@@ -0,0 +1,189 @@
+// 04/11/23 React practice class 
+
+
+'use strict';
+// the class acts as a blueprint for all the objects insteads of typing each object 1 at a time we can use class
+class Person {//extends ( this part needs to be used with super(props))
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
+
+// on javascript only we would be typing this in 3 lines and but this allows us to use class Person as a blueprint
+const newPerson = new Person("Shakirah", 23);
+const newPerson1 = new Person("Makhosi", 33);
+
+console.log(newPerson.name);
+
+class Student extends Person {
+    constructor(name, age, studentId) { // also add the props from Person here 
+        super(name, age) // need to add the properties of the Person so it can be outputted as well
+        this.studentId = studentId;
+    }
+}
+const student1 = new Student("Makhosi", 33, "T1234");
+console.log(student1.studentId);
+console.log(`My name is ${student1.name} and I am ${student1.age}. My student ID number is ${student1.studentId}`);
+
+class ListItems extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return React.createElement(
+            "ul",
+            {
+                style: {
+                    display: "inline-block",
+                    fontSize: "20px",
+                    color: "green"
+                },
+                id: "lists"
+            },
+
+            React.createElement("li", null, "Item 1"),
+            React.createElement("li", null, "Item 2"),
+            React.createElement("li", null, "Item 3")
+
+
+        )
+    }
+
+};
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0
+        };
+        this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
+        this.reset = this.reset.bind(this);
+    }
+
+   
+
+        increment() {
+        this.setState(prevState => ({
+            count: prevState.count + 1
+        }));
+    };
+    decrement() {
+        this.setState(prevState => ({
+            count: prevState.count - 1
+        }));
+    };
+
+    reset() {
+        this.setState(prevState => ({
+            count: prevState = 0
+        }));
+    };
+
+    render() {
+        const {count} = this.state;
+
+        return React.createElement(
+            "p",
+            {
+                style: {
+                    color: "green",
+                    fontWeight: "light",
+                    display: "flex",
+                    flexDirection: "row",
+
+                },
+
+            },
+            'You clicked ' + count + ' times',
+        
+
+
+            React.createElement(
+                "button",
+                {
+                    style:
+                    {
+                        color: "green",
+                        fontSize: "20px",
+                        fontWeight: "bold",
+                        backgroundColor: "cream",
+                        padding: "10px 20px",
+                        fontWeight: "bold",
+                        borderRadius: "20px",
+                        fontSize: "1.1em",
+                        margin: "20px"
+                        
+
+                    },
+                    onClick: () => this.setState({ count: this.state.count + 1 }),
+
+                },
+                "Increase"
+            ),
+
+            
+
+            React.createElement(
+                "button",
+                {
+                    style:
+                    {
+                        color: "green",
+                        fontSize: "20px",
+                        fontWeight: "bold",
+                        backgroundColor: "cream",
+                        padding: "10px 20px",
+                        fontWeight: "bold",
+                        borderRadius: "20px",
+                        fontSize: "1.1em",
+                        margin: "20px"
+
+                    },
+                    onClick: () => this.setState({ count: this.state.count - 1 }),
+
+                },
+                "Decrease"
+            ),
+
+            React.createElement(
+                "button",
+                {
+                    style:
+                    {
+                        color: "green",
+                        fontSize: "20px",
+                        fontWeight: "bold",
+                        backgroundColor: "cream",
+                        padding: "10px 20px",
+                        fontWeight: "bold",
+                        borderRadius: "20px",
+                        fontSize: "1.1em",
+                        margin: "20px"
+
+                    },
+                    onClick: () => this.setState({ count: this.state.count = 0 }),
+
+                },
+                "Reset"
+            )
+        
+
+        );
+            };
+}
+
+
+
+
+
+
+const domContainer = document.querySelector("#root");
+ReactDOM.render(React.createElement(ListItems), domContainer);
+
+
+ReactDOM.render(React.createElement(Counter), domContainer);
